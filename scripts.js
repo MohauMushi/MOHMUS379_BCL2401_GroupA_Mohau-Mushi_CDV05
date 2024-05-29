5f0013215fbe939ad4c3090e133fea6ba9d2278d
Mohau Mushi <david1mushi1@gmail.com>
2024-05-29 12:52:51 +0200

    Implement a functionality for toggling navigation menu to show, when the hamburger menu is pressed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".links");
+
+// Toggle the "show-links" class on navLinks container when the hamburger is clicked
+hamburger.addEventListener("click", () => {
+	navLinks.classList.toggle("show-links");
+  });
